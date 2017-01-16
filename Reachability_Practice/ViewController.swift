8f7564e5b64105e7e56b9a27b97ad6f55d78daa6
Pramod Shirsath <pramods@supraint.com>
2017-01-16 11:51:38 +0530

    Initial Commit

---
+++ Reachability_Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reachability_Practice
+//
+//  Created by Suraj MAC3 on 16/01/17.
+//  Copyright © 2017 Suraj MAC3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
